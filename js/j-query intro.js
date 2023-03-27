1108e2dc452b94f17372ee819ee4f46c9771827f
Jim Olson <jim.olson.456@gmail.com>
2023-03-27 09:59:22 -0500

    hello

---
+++ js/j-query intro.js
@@ -0,0 +1,3 @@
+$(function () {
+    alert('html has done loading');
+});
---
+++ js/practice.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+setInterval(displayHello, 1000);
+
+function displayHello() {
+    
+}
+
+
+
+
+
+
+//setInterval(function () {"Hello"}, 1000);
+//setInterval(displayHello, 1000);
+
+
+
+
+
+
+
+//var thre = "displayHello"
+//
+//setInterval(console.log(thre), 1000);
+
+
+
+
+
+
+
+
+
+//var countTo = 20
+//
+//var countTo2 = 1
+//
+//setInterval(countTo, 1000){
+//
+//}
